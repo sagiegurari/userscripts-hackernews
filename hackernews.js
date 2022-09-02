3148f3d8866f57dbd97f8fe1b5df4579fc5dcfc3
sagie gur ari <sagiegurari@gmail.com>
2022-09-02 05:12:27 +0000

    more styles, increase cache size and improve performance

--- hackernews.js
+++ hackernews.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hackernews Modern
 // @namespace    sagiegurari
-// @version      1.6
+// @version      1.7
 // @author       Sagie Gur-Ari
 // @description  Improved mobile usability and modern styling for Hackernews
 // @homepage     https://github.com/sagiegurari/userscripts-hackernews
@@ -27,7 +27,8 @@
         '#hnmain tr:first-child td, .comment-tree { background-color: #333; }',
         'html, body, #hnmain, #hnmain table.itemList tr:first-child td { background-color: #222; }',
         'a:link, .subtext a[href^="item"]:not(:visited), a:link.togg.clicky, .commtext, .comment-tree a[rel="nofollow"], .comment-tree .reply a { color: #eee; }',
-        '.visited a.titlelink { color: #888; }',
+        '.visited a.titlelink, .visited a:link, .visited .subtext a[href^="item"] { color: #888; }',
+        '.morelink { text-align: center; display: block; margin: 10px 40px 10px 0; background-color: #af4000; font-weight: bold; padding: 10px; }',
     ];
 
     // if mobile or emulator
@@ -50,101 +51,106 @@
         styleSheet.insertRule(cssRule, styleSheet.cssRules.length);
     });
 
-    // collapse non top comments
-    document.querySelectorAll('.ind:not([indent="0"])').forEach(topCommentIndent => {
-        topCommentIndent.parentElement.querySelectorAll('.togg.clicky').forEach(toggle => toggle.click());
-    });
+    const articlePage = window.location.search.indexOf('id=') !== -1;
 
-    // remove root/next/prev links
-    document.querySelectorAll('.navs').forEach(element => element.style.display = 'none');
-
-    const storage = window.localStorage;
-    if (storage && typeof storage.getItem === 'function') {
-        const KEY = 'hn-cache-visited';
-        const CACHE_LIMIT = 100;
-
-        const readFromCache = () => {
-            const listStr = storage.getItem(KEY);
-
-            if (!listStr) {
-                return [];
-            }
-
-            return listStr.split(',');
-        };
-        const writeToCache = (ids) => {
-            if (!ids || !Array.isArray(ids) || !ids.length) {
-                return;
-            }
-
-            // add to start
-            cache.unshift(...ids);
-
-            // remove duplicates
-            const seen = {};
-            cache = cache.filter(function (item) {
-                if (seen[item]) {
-                    return false;
-                }
-
-                seen[item] = true;
-                return true;
-            });
-
-            // trim
-            const extraCount = cache.length - CACHE_LIMIT;
-            if (extraCount) {
-                cache.splice(cache.length - extraCount, extraCount);
-            }
-
-            storage.setItem(KEY, cache.join(','));
-        };
-
-        let cache = readFromCache();
-
-        // mark visited
-        const markVisited = () => {
-            const elements = document.querySelectorAll('tr.athing');
-
-            for (let index = 0; index < elements.length; index++) {
-                const element = elements[index];
-                if (cache.indexOf(element.id) !== -1) {
-                    element.classList.add('visited');
-                }
-            }
-        };
-        markVisited();
-
-        // listen to scroll and add to cache
-        const markVisibleAsVisited = () => {
-            const elements = document.querySelectorAll('tr.athing:not(.visited)');
-
-            let started = false;
-            const ids = [];
-            for (let index = 0; index < elements.length; index++) {
-                const element = elements[index];
-                const bounding = element.getBoundingClientRect();
-                if (bounding.top >= 0 &&
-                    bounding.bottom <= window.innerHeight) {
-                    started = true;
-                    ids.push(element.id);
-                } else if (started) {
-                    break;
-                }
-            }
-
-            if (ids.length) {
-                writeToCache(ids);
-            }
-        };
-        let timeoutID = null;
-        document.addEventListener('scroll', () => {
-            clearTimeout(timeoutID);
-            timeoutID = setTimeout(markVisibleAsVisited, 25);
-        }, {
-            passive: true
+    if (articlePage) {
+        // collapse non top comments
+        document.querySelectorAll('.ind:not([indent="0"])').forEach(topCommentIndent => {
+            topCommentIndent.parentElement.querySelectorAll('.togg.clicky').forEach(toggle => toggle.click());
         });
 
-        markVisibleAsVisited();
+        // remove root/next/prev links
+        document.querySelectorAll('.navs').forEach(element => element.style.display = 'none');
+    } else {
+        const storage = window.localStorage;
+        if (storage && typeof storage.getItem === 'function') {
+            const KEY = 'hn-cache-visited';
+            const CACHE_LIMIT = 1000;
+
+            const readFromCache = () => {
+                const listStr = storage.getItem(KEY);
+
+                if (!listStr) {
+                    return [];
+                }
+
+                return listStr.split(',');
+            };
+            const writeToCache = (ids) => {
+                if (!ids || !Array.isArray(ids) || !ids.length) {
+                    return;
+                }
+
+                // add to start
+                cache.unshift(...ids);
+
+                // remove duplicates
+                const seen = {};
+                cache = cache.filter(function (item) {
+                    if (seen[item]) {
+                        return false;
+                    }
+
+                    seen[item] = true;
+                    return true;
+                });
+
+                // trim
+                const extraCount = cache.length - CACHE_LIMIT;
+                if (extraCount) {
+                    cache.splice(cache.length - extraCount, extraCount);
+                }
+
+                storage.setItem(KEY, cache.join(','));
+            };
+
+            let cache = readFromCache();
+
+            // mark visited
+            const markVisited = () => {
+                document.querySelectorAll('tr.athing').forEach(element => {
+                    if (cache.indexOf(element.id) !== -1) {
+                        element.classList.add('visited');
+                    }
+                });
+
+                document.querySelectorAll('tr.visited + tr').forEach(element => {
+                    element.classList.add('visited');
+                });
+            };
+            markVisited();
+
+            // listen to scroll and add to cache
+            const markVisibleAsVisited = () => {
+                const elements = document.querySelectorAll('tr.athing:not(.visited)');
+
+                let started = false;
+                const ids = [];
+                for (let index = 0; index < elements.length; index++) {
+                    const element = elements[index];
+                    const bounding = element.getBoundingClientRect();
+                    if (bounding.top >= 0 &&
+                        bounding.bottom <= window.innerHeight) {
+                        started = true;
+                        ids.push(element.id);
+                    } else if (started) {
+                        break;
+                    }
+                }
+
+                if (ids.length) {
+                    writeToCache(ids);
+                }
+            };
+            let timeoutID = null;
+            document.addEventListener('scroll', () => {
+                clearTimeout(timeoutID);
+                timeoutID = setTimeout(markVisibleAsVisited, 25);
+            }, {
+                passive: true
+            });
+
+            markVisibleAsVisited();
+        }
     }
 }());
